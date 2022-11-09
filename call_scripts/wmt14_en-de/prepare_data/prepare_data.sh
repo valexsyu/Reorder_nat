76d622fb85f075da054edee83cd42f0ebd86d546
valexsyu <valexsyu@foxmail.com>
2022-11-09 16:22:14 +0800

    modify path

--- call_scripts/wmt14_en-de/prepare_data/prepare_data.sh
+++ call_scripts/wmt14_en-de/prepare_data/prepare_data.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 source $HOME/.bashrc 
 conda activate bibert
-bash /home/valexsyu/Doc/NMT/Reorder_nat/call_scripts/wmt14_en-de/prepare_data/BiBert/download_and_prepare_data.sh
+# bash /home/valexsyu/Doc/NMT/Reorder_nat/call_scripts/wmt14_en-de/prepare_data/BiBert/download_and_prepare_data.sh
 
 bash /home/valexsyu/Doc/NMT/Reorder_nat/call_scripts/wmt14_en-de/prepare_data/fairseq_preprocess.sh
