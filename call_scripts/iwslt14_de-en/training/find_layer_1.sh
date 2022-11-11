af3becb2c33e21197a6590ff06a12b4c84bb069e
valexsyu <valexsyu@foxmail.com>
2022-11-11 19:13:22 +0800

    add file

--- call_scripts/iwslt14_de-en/training/find_layer_1.sh
+++ call_scripts/iwslt14_de-en/training/find_layer_1.sh
@@ -2,4 +2,4 @@ source $HOME/.bashrc
 conda activate base
 
 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 1-1-1-1-H11-UF20M 
-bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR45M
+bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR15M --fp16 --save-interval-updates 70000 --max-tokens 3072
