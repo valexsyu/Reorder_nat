52d94f83dab3ff803dc7d153f25bec5e981211f0
valexsyu <valexsyu@foxmail.com>
2022-11-13 21:39:40 +0800

    add -g 8

--- call_scripts/iwslt14_de-en/training/E-2-1-1-H12-UR40M-twcc.sh
+++ call_scripts/iwslt14_de-en/training/E-2-1-1-H12-UR40M-twcc.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e E-2-1-1-H12-UR40M --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 2048 -b 65536
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e E-2-1-1-H12-UR40M --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 2048 -b 65536 -g 8
