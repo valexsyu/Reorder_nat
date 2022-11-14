38318411a0c7ead96dc7c2c0a6b66bf10900bb72
valexsyu <valexsyu@foxmail.com>
2022-11-14 08:25:13 +0800

    smaller --max-tokens 2048

--- call_scripts/iwslt14_de-en/training/2-2-1-1-N-UD50M-twcc.sh
+++ call_scripts/iwslt14_de-en/training/2-2-1-1-N-UD50M-twcc.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-N-UD50M --fp16 --save-interval-updates 70000 --twcc --max-tokens 3072
+CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-N-UD50M --fp16 --save-interval-updates 70000 --twcc --max-tokens 2048
