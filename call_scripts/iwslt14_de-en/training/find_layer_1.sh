c7d6a39cfe75b0315c5ae18df4fe2f490edb767a
valexsyu <valexsyu@foxmail.com>
2022-11-16 00:34:10 +0800

    rename dataset and add others

--- call_scripts/iwslt14_de-en/training/find_layer_1.sh
+++ call_scripts/iwslt14_de-en/training/find_layer_1.sh
@@ -8,5 +8,8 @@ conda activate base
 
 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-N-UF50M --fp16 --save-interval-updates 70000
 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-N-UF50M
+bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H7-UF20M --fp16 --save-interval-updates 70000
+bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H7-UF20M
+
 # bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H6-UF20T --save-interval-updates 70000
 # bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H6-UF20T
