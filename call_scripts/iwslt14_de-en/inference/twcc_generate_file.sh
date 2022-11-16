05fed7cee4df2afa093c46d0da8e9d69d3d9c328
valexsyu <valexsyu@foxmail.com>
2022-11-16 23:29:28 +0800

    add twcc file

--- call_scripts/iwslt14_de-en/inference/twcc_generate_file.sh
+++ call_scripts/iwslt14_de-en/inference/twcc_generate_file.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e C-1-1-1-H12-UF30M --twcc -b 60
+CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e C-1-1-1-H12-UF30M -e 2-2-1-1-H12-UD50T --twcc -b 60
