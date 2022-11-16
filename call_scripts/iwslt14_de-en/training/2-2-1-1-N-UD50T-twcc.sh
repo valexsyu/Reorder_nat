a63164783b44cf90a0645d052cf6248cc3c9d534
valexsyu <valexsyu@foxmail.com>
2022-11-16 21:08:45 +0800

    add twcc files

--- call_scripts/iwslt14_de-en/training/2-2-1-1-N-UD50T-twcc.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UD50T --fp16 --save-interval-updates 70000 --twcc --max-tokens 2048
--- call_scripts/iwslt14_de-en/training/train_battleship_4g.sh
+++ call_scripts/iwslt14_de-en/training/train_battleship_4g.sh
@@ -3,5 +3,7 @@ conda activate base
 
 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H7-UR40M --fp16 -g 4 --save-interval-updates 70000 --max-tokens 1536
 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H7-UR40M
+bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H7-UR40M --fp16 -g 4 --save-interval-updates 70000 --max-tokens 1536 --dropout 0.3
+bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H7-UR40M
 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e F-2-1-1-N-UR40M --fp16 -g 4 --save-interval-updates 70000 --max-tokens 2048 -b 65536
 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e F-2-1-1-N-UR40M 
