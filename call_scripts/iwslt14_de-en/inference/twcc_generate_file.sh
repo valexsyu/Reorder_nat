cdac01295c9b8ab30673118dd3436f28f8575728
valexsyu <valexsyu@foxmail.com>
2022-11-16 16:42:01 +0800

    add twcc generate_file.sh

---
+++ call_scripts/iwslt14_de-en/inference/twcc_generate_file.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e C-1-1-1-H12-UF30M -e 2-2-1-1-N-UD50M \
+-e 2-2-1-1-H12-UD50M -e 2-2-1-1-H12-UR50M --twcc 
--- call_scripts/iwslt14_de-en/training/2-2-1-1-H12-UxxxM.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIABLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR25M --twcc --fp16 --save-interval-updates 70000 --max-tokens 3072
-CUDA_VISIABLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR22M --twcc --fp16 --save-interval-updates 70000 --max-tokens 3072
-CUDA_VISIABLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR20M --twcc --fp16 --save-interval-updates 70000 --max-tokens 4096
-# CUDA_VISIABLE_DEVICES=0 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UR15M --twcc --fp16 --save-interval-updates 70000 --max-tokens 4096
-
-CUDA_VISIABLE_DEVICES=0 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-N-UD45T -e 2-2-1-1-N-UD40T -e 2-2-1-1-N-UD45M -e 2-2-1-1-H12-UD45M -e 2-2-1-1-H12-UR22M -e 2-2-1-1-H12-UR20M
-echo "========================end========================="
