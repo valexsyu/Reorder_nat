b61aa5ae30cb81e2576b83e625b06b652ce28c4e
valexsyu <valexsyu@foxmail.com>
2022-11-17 12:14:46 +0800

    fix name

--- call_scripts/iwslt14_de-en/training/2-2-5-1-N-UF30T-twcc.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-5-1-N-UF30T --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 4 --dropout 0.1
