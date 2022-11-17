2d20812b4fdcd93f06cebe643b1662630cd6d19b
valexsyu <valexsyu@foxmail.com>
2022-11-17 11:20:08 +0800

    add twcc files

---
+++ call_scripts/iwslt14_de-en/training/2-2-1-1-N-UR40T-twcc.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-N-UR40T --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 4 --dropout 0.1
--- call_scripts/iwslt14_de-en/training/find_layer.sh
+++ call_scripts/iwslt14_de-en/training/find_layer.sh
@@ -17,8 +17,8 @@ conda activate base
 
 # bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H11-UF20M -e 2-2-1-1-H7-UF20M -e 1-1-1-1-H11-UF20T -e 1-1-1-1-H9-UF20T -e 1-1-1-1-H7-UF20T
 
-bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UF50M --fp16 --save-interval-updates 70000
-bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H12-UF50M
+# bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 2-2-1-1-H12-UF50M --fp16 --save-interval-updates 70000
+# bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 2-2-1-1-H12-UF50M
 # bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 1-1-1-1-H6-UF20M --fp16 --save-interval-updates 70000
 # bash call_scripts/iwslt14_de-en/inference/generate_nat.sh -e 1-1-1-1-H6-UF20M
 bash call_scripts/iwslt14_de-en/training/train_nat.sh -e 1-1-1-1-H12-UF20M --fp16 --save-interval-updates 70000
