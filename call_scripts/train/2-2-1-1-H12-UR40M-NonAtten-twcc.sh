1bf79e66bcf8ec2eae9e7217eae9490974b3b88e
valexsyu <valexsyu@foxmail.com>
2022-11-19 22:04:53 +0800

    test for add atten_mask

--- call_scripts/train/2-2-1-1-H12-UR40M-NonAtten-twcc.sh
+++ call_scripts/train/2-2-1-1-H12-UR40M-NonAtten-twcc.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 bash call_scripts/train_nat.sh -e 2-2-1-1-H12-UR40M --twcc --fp16 \
+CUDA_VISIBLE_DEVICES=0,1 bash call_scripts/train_nat.sh -e 2-2-1-1-H12-UR40M --twcc --fp16 \
                             --save-interval-updates 70000 --max-update 100000 --max-tokens 1536 -b 12288 -g 2 --dropout 0.1
-CUDA_VISIBLE_DEVICES=0 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh --twcc -b 60 -e 2-2-1-1-H12-UR40M
+CUDA_VISIBLE_DEVICES=0,1 bash call_scripts/iwslt14_de-en/inference/generate_nat.sh --twcc -b 60 -e 2-2-1-1-H12-UR40M
