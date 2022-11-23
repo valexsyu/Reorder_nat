9c24e563112e05a6fa43640b1d4b679833e463cc
valexsyu <valexsyu@foxmail.com>
2022-11-24 02:30:22 +0800

    add diffbatch test

--- call_scripts/generate/generate_batch_twcc.sh
+++ call_scripts/generate/generate_batch_twcc.sh
@@ -3,6 +3,11 @@
 CUDA_VISIBLE_DEVICES=0 bash call_scripts/generate_nat_diffbatch.sh -b 50 --data-subset test-valid \
     --ck-types top --no-atten-mask --avg-ck-turnoff -twcc \
 -e 2-2-1-1-H12-UR15M \
+-e 2-2-1-1-H12-UR20M \
+-e 2-2-1-1-H12-UR22M \
+-e 2-2-1-1-H12-UR25M \
 -e 2-2-1-1-H12-UR30M \
 -e 2-2-1-1-H12-UR33M \
+-e 2-2-1-1-H12-UR40M \
 -e 2-2-1-1-H12-UR45M \
+-e 2-2-1-1-H12-UR50M \
