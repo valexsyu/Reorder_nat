bc6b538e760e8a42228591af5f94a8399ac45dcf
valexsyu <valexsyu@foxmail.com>
2022-11-23 15:17:08 +0800

    add file

--- call_scripts/train/2-2-1-1-H12-UF50T-twcc.sh
+++ call_scripts/train/2-2-1-1-H12-UF50T-twcc.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1,2,4 bash call_scripts/train_nat.sh -e 2-2-1-1-H12-UF50T --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 4 --dropout 0.1
-CUDA_VISIBLE_DEVICES=0,1,2,4 bash call_scripts/generate_nat.sh -e 2-2-1-1-H12-UF50T --twcc -b 50 --data-subset test-valid --no-atten-mask 
-CUDA_VISIBLE_DEVICES=0,1,2,4 bash call_scripts/generate_nat.sh -e 2-2-1-1-H12-UF50T --twcc -b 80 --data-subset test-valid --avg-ck-turnoff
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/train_nat.sh -e 2-2-1-1-H12-UF50T --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 4 --dropout 0.1
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/generate_nat.sh -e 2-2-1-1-H12-UF50T --twcc -b 50 --data-subset test-valid --no-atten-mask 
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/generate_nat.sh -e 2-2-1-1-H12-UF50T --twcc -b 80 --data-subset test-valid --avg-ck-turnoff
