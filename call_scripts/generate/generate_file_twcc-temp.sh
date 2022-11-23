8bf03f77fecf30d3fa17e92f06768d137b7437ef
valexsyu <valexsyu@foxmail.com>
2022-11-23 22:35:03 +0800

    add file

--- call_scripts/generate/generate_file_twcc-temp.sh
+++ call_scripts/generate/generate_file_twcc-temp.sh
@@ -2,7 +2,7 @@
 bash call_scripts/generate_nat.sh -b 50 --data-subset test-valid --no-atten-mask --twcc --avg-ck-turnoff \
 -e 1-1-1-1-N-UF20T \
 -e 1-1-1-1-H12-UF20T \
-
+ 
 # CUDA_VISIBLE_DEVICES=0 bash call_scripts/generate_nat.sh -b 100 --data-subset test-valid --avg-ck-turnoff --twcc \
 # -e 1-1-1-1-N-UF20T \
 # -e 1-1-1-1-H12-UF20T \
