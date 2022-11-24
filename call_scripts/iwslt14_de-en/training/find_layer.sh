09c69cb52b73667f2f39a4bb3a276f003ea62c3f
valexsyu <valexsyu@foxmail.com>
2022-11-24 21:30:48 +0800

    add file

--- call_scripts/iwslt14_de-en/training/find_layer.sh
+++ call_scripts/iwslt14_de-en/training/find_layer.sh
@@ -38,6 +38,14 @@ bash call_scripts/train_nat.sh -e J-6-1-1-N-UF30T --fp16 --save-interval-updates
 bash call_scripts/generate_nat.sh -e J-6-1-1-N-UF30T --data-subset test-valid
 bash call_scripts/train_nat.sh -e K-2-1-1-H12-UR40M --fp16 --save-interval-updates 70000 --max-tokens 2048
 bash call_scripts/generate_nat.sh -e K-2-1-1-H12-UR40M --data-subset test-valid
+bash call_scripts/train_nat.sh -e 2-6-1-1-N-UF30T --fp16 --save-interval-updates 70000 --max-tokens 2048 --no-atten-mask
+bash call_scripts/generate_nat.sh -e 2-6-1-1-N-UF30T -b 50 --data-subset test-valid --no-atten-mask
+bash call_scripts/train_nat.sh -e J-6-1-1-N-UF30T --fp16 --save-interval-updates 70000 --max-tokens 2048 --no-atten-mask
+bash call_scripts/generate_nat.sh -e J-6-1-1-N-UF30T -b 50 --data-subset test-valid --no-atten-mask
+bash call_scripts/train_nat.sh -e K-2-1-1-H12-UF30M --fp16 --save-interval-updates 70000 --max-tokens 2048 --no-atten-mask
+bash call_scripts/generate_nat.sh -e K-2-1-1-H12-UF30M -b 50 --data-subset test-valid --no-atten-mask
+bash call_scripts/train_nat.sh -e K-2-1-1-H12-UR40M --fp16 --save-interval-updates 70000 --max-tokens 2048 --no-atten-mask
+bash call_scripts/generate_nat.sh -e K-2-1-1-H12-UR40M -b 50 --data-subset test-valid --no-atten-mask
 
 
 
