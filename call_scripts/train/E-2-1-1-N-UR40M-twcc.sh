741e3778c4e1d4270f0c513cd40ff7df526da0bf
valexsyu <valexsyu@foxmail.com>
2022-11-28 23:53:08 +0800

    add file

---
+++ call_scripts/train/E-2-1-1-N-UR40M-twcc.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash call_scripts/train_nat.sh -e E-2-1-1-N-UR40M --fp16 -g 4 --save-interval-updates 32500 --max-update 200000 --lm-start-step 130000 --max-tokens 2048 -b 65536 --no-atten-mask
