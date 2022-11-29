6111991bedfec69fa01eb40336f3b3d1b69c179f
valexsyu <valexsyu@foxmail.com>
2022-11-30 00:28:50 +0800

    add file

--- call_scripts/generate/generate_file-twcc.sh
+++ call_scripts/generate/generate_file-twcc.sh
@@ -1,7 +1,7 @@
 #--------iwslt14 deen main table------
 CUDA_VISIBLE_DEVICES=0 bash call_scripts/generate_nat.sh -b 1 \
     --data-subset test-valid --no-atten-mask \
-    --avg-speed 1 -twcc \
+    --avg-speed 1 --twcc \
     -e K-2-1-1-H12-UR40M -e K-2-1-1-N-UR40M -e K-2-1-1-N-UR40T -e K-2-1-1-N-UF30T -e K-6-1-1-N-UF30T \
     -e I-6-1-1-N-UF30T -e 2-2-1-1-H12-UR40M -e 2-2-1-1-N-UR40M -e 2-2-1-1-N-UR40T -e 2-2-1-1-N-UF30T \
     -e 2-6-1-1-N-UF30T -e J-6-1-1-N-UF30T -e K-2-1-1-H12-UD25M -e 2-2-1-1-H12-UD25M \
