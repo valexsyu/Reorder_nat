403bebc5d24cd6785663ba92c0b38b0eb651ae40
valexsyu <valexsyu@foxmail.com>
2022-12-11 21:37:58 +0800

    add wmt14 deen clean dataset

--- call_scripts/wmt14_de_en/prepare_data/BiBert/download_and_prepare_data.sh
+++ call_scripts/wmt14_de_en/prepare_data/BiBert/download_and_prepare_data.sh
@@ -49,8 +49,43 @@ conda activate bibert
 # cp $TOKEN_PATH/* /home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/wmt14_en_de_token_bibert/
 
 
-INPUT_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/baseline_detoken_distilled_wmt14_de_en
-TOKEN_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/baseline_token_distilled_wmt14_de_en_bibert
+# INPUT_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/baseline_detoken_distilled_wmt14_de_en
+# TOKEN_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/baseline_token_distilled_wmt14_de_en_bibert
+# MODEL_NAME=jhu-clsp/bibert-ende
+# ## tokenize translation data
+# mkdir $TOKEN_PATH
+# cd /home/valexsyu/Doc/NMT/Reorder_nat/call_scripts/wmt14_de_en/prepare_data/BiBert
+
+
+# SCRIPTS=/home/valexsyu/Doc/NMT/Reorder_nat/call_scripts/tool/mosesdecoder/scripts
+
+# ## de-mose data
+# mkdir $INPUT_PATH/demose
+# for prefix in "valid" "test" "train" ;
+# do
+#     for lang in "de" "en" ;
+#     do
+#         perl $SCRIPTS/tokenizer/detokenizer.perl -l en -q < $INPUT_PATH/${prefix}.${lang} > $INPUT_PATH/demose/${prefix}.${lang}
+#     done
+# done
+
+# for prefix in "valid" "test" "train" ;
+# do
+#     for lang in "en" "de" ;
+#     do
+#         python transform_tokenize.py --input $INPUT_PATH/demose/${prefix}.${lang} --output $TOKEN_PATH/${prefix}.${lang} --pretrained_model $MODEL_NAME
+#     done
+# done
+
+# ## get src and tgt vocabulary
+# python get_vocab.py --tokenizer jhu-clsp/bibert-ende --output ${TOKEN_PATH}/src_vocab.txt
+# cp $TOKEN_PATH/src_vocab.txt $TOKEN_PATH/tgt_vocab.txt
+
+
+
+INPUT_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/wmt14_de_en_detoken_clean
+TOKEN_PATH=/home/valexsyu/Doc/NMT/Reorder_nat/data/nat_position_reorder/awesome/wmt14_de_en_token_clean_bibert
+
 MODEL_NAME=jhu-clsp/bibert-ende
 ## tokenize translation data
 mkdir $TOKEN_PATH
@@ -80,5 +115,7 @@ done
 ## get src and tgt vocabulary
 python get_vocab.py --tokenizer jhu-clsp/bibert-ende --output ${TOKEN_PATH}/src_vocab.txt
 cp $TOKEN_PATH/src_vocab.txt $TOKEN_PATH/tgt_vocab.txt
+
+
 
 
