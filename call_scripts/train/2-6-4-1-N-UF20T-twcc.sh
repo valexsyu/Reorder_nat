0bb44f16475d768eb380ca1c84bdabcb1fe2b0ba
valexsyu <valexsyu@foxmail.com>
2023-01-05 19:38:50 +0800

    add file

--- call_scripts/train/2-6-4-1-N-UF20T-twcc.sh
+++ call_scripts/train/2-6-4-1-N-UF20T-twcc.sh
@@ -8,3 +8,10 @@ cp checkpoints/$experiment_1/top5_700000steps/* checkpoints/$experiment_2/
 cp checkpoints/$experiment_1/checkpoint_last.pt checkpoints/$experiment_2/
 CUDA_VISIBLE_DEVICES=0 bash call_scripts/train_nat.sh -e $experiment_1 --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 1 --dropout 0.1 --no-atten-mask
 CUDA_VISIBLE_DEVICES=0 bash call_scripts/train_nat.sh -e $experiment_2 --twcc --fp16 --save-interval-updates 70000 --max-update 100000 --max-tokens 3072 -b 12288 -g 1 --dropout 0.1 --no-atten-mask
+
+CUDA_VISIBLE_DEVICES=0 bash call_scripts/generate_nat -b 1 --twcc --data-subset test --ck-types top --avg-speed 1 --no-atten-mask \
+                    -e $experiment_1 \
+                    -e $experiment_2 \
+
+
+#done
