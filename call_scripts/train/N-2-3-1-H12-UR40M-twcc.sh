1b4461b5e061df83b685c2f16741d7514b5edf40
valexsyu <valexsyu@foxmail.com>
2023-05-31 14:55:47 +0800

    N2-3-1-H12-UR40M TWCC

---
+++ call_scripts/train/N-2-3-1-H12-UR40M-twcc.sh
@@ -0,0 +1,7 @@
+source call_scripts/train/pair_experiment.sh
+pair_experiment_wmt14_2_3276_100k_twcc N-2-3-1-H12-UR40M       
+
+CUDA_VISIBLE_DEVICES=0,1 bash call_scripts/generate_nat.sh -b 1 --twcc --data-subset test --ck-types top --avg-speed 1 --no-atten-mask \
+                    -e $experiment_1 \
+                    
+                    
