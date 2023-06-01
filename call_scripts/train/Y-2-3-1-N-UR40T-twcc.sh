64d9a97ab17e96dfe97e3c8efeb936b0e37d1a9e
valexsyu <valexsyu@foxmail.com>
2023-06-01 10:17:48 +0800

    add new twcc task

---
+++ call_scripts/train/Y-2-3-1-N-UR40T-twcc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# source $HOME/.bashrc 
+# conda activate base
+source call_scripts/train/pair_experiment.sh
+experiment=Y-2-3-1-N-UR40T
+
+pair_experiment_wmt14_2_3276_100k_twcc $experiment       
+
+CUDA_VISIBLE_DEVICES=0,1 bash call_scripts/generate_nat.sh -b 1 --twcc --data-subset test --ck-types top --avg-speed 1 --no-atten-mask \
+                    -e $experiment   \
+
+            
