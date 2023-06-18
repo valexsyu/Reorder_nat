f7102b00780c780ff115861fdb0c2f495062c3c4
valexsyu <valexsyu@foxmail.com>
2023-06-18 17:23:09 +0800

    twcc

--- call_scripts/train/s-F-3-1-N-UR30M-100k_300k-twcc.sh
+++ call_scripts/train/s-F-3-1-N-UR30M-100k_300k-twcc.sh
@@ -4,8 +4,8 @@
 source call_scripts/train/pair_experiment.sh
 experiment=s-F-3-1-N-UR30M-100k_300k
 
-mkdir checkpoints/s-F-3-1-N-UR30M-100k_300k
-cp -r checkpoints/s-F-3-1-N-UR30M/checkpoint_last.pt checkpoints/s-F-3-1-N-UR30M-100k_300k/
+# mkdir checkpoints/s-F-3-1-N-UR30M-100k_300k
+# cp -r checkpoints/s-F-3-1-N-UR30M/checkpoint_last.pt checkpoints/s-F-3-1-N-UR30M-100k_300k/
 pair_experiment_wmt14_8_2730_100k_300k_twcc $experiment       
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash call_scripts/generate_nat.sh -b 10 --twcc --data-subset test --ck-types top --avg-speed 1 \
