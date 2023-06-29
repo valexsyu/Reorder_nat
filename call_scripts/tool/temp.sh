18fd4ecb9a3113083965b8b066a920925724d1b5
valexsyu <valexsyu@foxmail.com>
2023-06-29 12:43:09 +0800

    twcc

--- call_scripts/tool/temp.sh
+++ call_scripts/tool/temp.sh
@@ -8,15 +8,15 @@ tmux split-window -v
 
 # #=============2==================
 # #Set the size of each pane to be the same
-# tmux resize-pane -t 0 -y 20%
-# tmux resize-pane -t 1 -y 80%
+tmux resize-pane -t 0 -y 20%
+tmux resize-pane -t 1 -y 80%
 
 # #==========3==================
 # Split the second window vertically
-tmux split-window -v
-tmux resize-pane -t 0 -y 10%
-tmux resize-pane -t 1 -y 10%
-tmux resize-pane -t 2 -y 70%
+# tmux split-window -v
+# tmux resize-pane -t 0 -y 10%
+# tmux resize-pane -t 1 -y 10%
+# tmux resize-pane -t 2 -y 70%
 
 # #============4==================
 # # Split the second window vertically
@@ -34,14 +34,14 @@ tmux resize-pane -t 2 -y 70%
 Select the first window and execute the first script
 tmux select-pane -t 0
 tmux send-keys "bash call_scripts/tool/watch-test-best5record-twcc.sh \
-    -e r-E-3-1-N-UR30M-rate_avg-33k_100k \
-    -e r-E-3-1-N-UR20M-100k_300k \
-    -e r-E-3-1-N-UR30M-100k_300k \
-    -e r-E-3-1-N-UR40M-100k_300k \
+    -e Z-2-3-1-N-UR30M-rate_avg-33k \
+    -e Z-2-3-1-N-UR20M-100k_300k \
+    -e Z-2-3-1-N-UR30M-100k_300k \
+    -e Z-2-3-1-N-UR40M-100k_300k \
     --sleep 120" C-m
 
-tmux select-pane -t 1
-tmux send-keys "conda activate reorder_nat" C-m
+# tmux select-pane -t 1
+# tmux send-keys "conda activate reorder_nat" C-m
 # tmux send-keys "bash call_scripts/tool/watch-test-polling.sh \
 #     --local \
 #     --task translation_ctcpmlm \
@@ -53,32 +53,29 @@ tmux send-keys "conda activate reorder_nat" C-m
 #     -e 2-2-3-1-N-UR30M-rate_avg-100k \
 #     --sleep 360" C-m
 
-Select the second window and execute the second script
-tmux select-pane -t 1
-tmux send-keys "conda activate reorder_nat" C-m
-tmux send-keys "bash call_scripts/tool/watch-test-polling.sh \
-    --local \
-    --task translation_ctcpmlm \
-    --arch nat_pretrained_model \
-    --criterion nat_ctc_loss \
-    -b 10 \
-    --gpu_id 1 \
-    -e K-2-3-1-N-UR20M \
-    -e K-2-3-1-N-UR30M \
-    -e K-2-3-1-N-UR40M \
-    --sleep 120" C-m
+# Select the second window and execute the second script
+# tmux select-pane -t 1
+# tmux send-keys "conda activate reorder_nat" C-m
+# tmux send-keys "bash call_scripts/tool/watch-test-polling.sh \
+#     --local \
+#     --task translation_ctcpmlm \
+#     --arch nat_pretrained_model \
+#     --criterion nat_ctc_loss \
+#     -b 10 \
+#     --gpu_id 1 \
+#     -e K-2-3-1-N-UR20M \
+#     -e K-2-3-1-N-UR30M \
+#     -e K-2-3-1-N-UR40M \
+#     --sleep 120" C-m
 
 # Select the third window and execute the third script
-tmux select-pane -t 2
+tmux select-pane -t 1
 tmux send-keys "conda activate reorder_nat" C-m
 tmux send-keys "bash call_scripts/tool/look_exist_best_5.sh \
-    -e r-E-3-1-N-UR30M-rate_avg-33k_100k \
-    -e r-E-3-1-N-UR20M-100k_300k \
-    -e r-E-3-1-N-UR30M-100k_300k \
-    -e r-E-3-1-N-UR40M-100k_300k \
-    -e K-2-3-1-N-UR20M \
-    -e K-2-3-1-N-UR30M \
-    -e K-2-3-1-N-UR40M \
+    -e Z-2-3-1-N-UR30M-rate_avg-33k \
+    -e Z-2-3-1-N-UR20M-100k_300k \
+    -e Z-2-3-1-N-UR30M-100k_300k \
+    -e Z-2-3-1-N-UR40M-100k_300k \
     --sleep 120" C-m
 
 # Attach to the tmux session to view the windows
--- call_scripts/train/Z-2-3-1-N-UR30M-rate_avg-33k-twcc.sh
+++ call_scripts/train/Z-2-3-1-N-UR30M-rate_avg-33k-twcc.sh
@@ -2,9 +2,9 @@
 # source $HOME/.bashrc 
 # conda activate base
 source call_scripts/train/pair_experiment.sh
-experiment=Z-2-3-1-N-UR30M-rate_sel-33k
+experiment=Z-2-3-1-N-UR30M-rate_avg-33k
 
-pair_experiment_wmt14_8_1638_rate_sel_33k_twcc $experiment       
+pair_experiment_wmt14_8_1638_rate_avg_33k_twcc $experiment       
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash call_scripts/generate_nat.sh -b 10 --twcc --data-subset test --ck-types top --avg-speed 1 \
                     -e $experiment   \
